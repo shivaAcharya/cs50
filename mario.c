24ad9a95fef2a467866bfd2111eedc025b8c51df
Shiva Acharya <cva.acharya8@gmail.com>
2021-08-23 23:21:42 -0700

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
-#include <string.h>
+#include <cs50.h>
 
 int main(void)
 {
-    int a = 2;
-    // printf("Please input an interger value: ");
-    // scanf("%i", &a);
-    char space [] = " ";
-    char hash[] = "#";
+    char space = ' ';
+    char pound = '#';
+    int height;
 
-    int k = a;
-    for (int i = 0; i < a; i++)
-    {    
-        for(int i = 1; i < k; i++)
+    // Prompt user to input height in between 1 and 8.
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    int a = height;
+
+    // Iterate over given height.
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over each line
+        for (int j = 1; j < a; j++)
         {
-            printf("%s", space);   
+            printf("%c", space);
         }
-        for (int i = a; i >= k; i--)
+
+        for (int k = height; k >= a; k--)
         {
-            printf("%s", hash);
+            printf("%c", pound);
         }
-        k--;
-        if (k > 0)
+
+        // Check if new line is needed.
+        if (a > 0)
         {
             printf("\n");
         }
-        
+        a--;
     }
-
 }
